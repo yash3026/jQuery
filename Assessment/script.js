9e3d0a3152a8614ca9b905397dced183b6b15b57
yash3026 <154597815+yash3026@users.noreply.github.com>
2024-04-03 16:33:30 +0530

    Add files via upload

---
+++ Assessment/script.js
@@ -0,0 +1,11 @@
+document.getElementById("grandparent").addEventListener("click", function() {
+    console.log("Tickle the grandparent!");
+  });
+  
+  document.getElementById("parent").addEventListener("click", function() {
+    console.log("Tickle the parent!");
+  });
+  
+  document.getElementById("child").addEventListener("click", function() {
+    console.log("Tickle the child!");
+  });
